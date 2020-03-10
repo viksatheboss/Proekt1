36f5e69e784d5003b68ae12d78fcf5b1caef2fbf
viksatheboss <61952531+viksatheboss@users.noreply.github.com>
2020-03-10 11:05:25 +0100

    Add files via upload

---
+++ Repository.Layer/KorisniciRepository.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModels.Layer;
+
+namespace Repository.Layer
+{
+     public interface IKorisniciRepository
+    {
+        void InsertUser(Korisnik u);
+        void UpdateUserDetails(Korisnik u);
+        void UpdateUserPassword(Korisnik u);
+        void DeleteUser(int uid);
+        List<Korisnik> GetUsers();
+        List<Korisnik> GetUsersByEmailAndPassword(string Email, string Password);
+        List<Korisnik> GetUsersByEmail(string Email);
+        List<Korisnik> GetUsersByUserId(int UserId);
+        int GetLatestUserId();
+
+    }
+     public class KorisniciRepository : IKorisniciRepository
+    {
+         readonly StoredVehiclesDatabaseDbContext dbcontext;
+
+        public KorisniciRepository()
+        {
+            dbcontext = new StoredVehiclesDatabaseDbContext();
+        }
+
+        public void InsertUser(Korisnik u)
+        {
+            dbcontext.Korisnici.Add(u);
+            dbcontext.SaveChanges();
+        }
+
+        public void UpdateUserDetails(Korisnik u)
+        {
+            Korisnik us = dbcontext.Korisnici.Where(k => k.UserId == k.UserId).FirstOrDefault();
+            if (us != null)
+            {
+                us.Name = u.Name;
+                us.Mobile = u.Mobile;
+                dbcontext.SaveChanges();
+            }
+        }
+
+        public void UpdateUserPassword(Korisnik u)
+        {
+            Korisnik us = dbcontext.Korisnici.Where(k => k.UserId == k.UserId).FirstOrDefault();
+            if(us != null)
+            {
+                us.PasswordHash = u.PasswordHash;
+                dbcontext.SaveChanges();
+            }
+        }
+
+        public void DeleteUser(int uid)
+        {
+            Korisnik us = dbcontext.Korisnici.Where(k => k.UserId == uid).FirstOrDefault();
+            if (us != null)
+            {
+                dbcontext.Korisnici.Remove(us);
+                dbcontext.SaveChanges();
+            }
+        }
+
+        public List<Korisnik> GetUsers()
+        {
+            List<Korisnik> us = dbcontext.Korisnici.Where(k => k.IsAdmin == false).OrderBy(k => k.Name).ToList();
+            return us;
+        }
+
+        public List<Korisnik> GetUsersByEmailAndPassword(string Email, string PasswordHash)
+        {
+            List<Korisnik> us = dbcontext.Korisnici.Where(k => k.Email == Email && k.PasswordHash == PasswordHash).ToList();
+            return us;
+        }
+
+        public List<Korisnik> GetUsersByEmail(string Email)
+        {
+            List<Korisnik> us = dbcontext.Korisnici.Where(k => k.Email == Email).ToList();
+            return us;
+        }
+
+        public List<Korisnik> GetUsersByUserId (int UserId)
+        {
+            List<Korisnik> us = dbcontext.Korisnici.Where(k => k.UserId == UserId).ToList();
+            return us;
+        }
+        public int GetLatestUserId()
+        {
+            int uid = dbcontext.Korisnici.Select(k => k.UserId).Max();
+            return uid;
+        }
+
+    }
+}
---
+++ Repository.Layer/MarkaNaVoziloRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModels.Layer;
+
+namespace Repository.Layer
+{
+    public interface IMarkaNaVoziloRepository
+    {
+        void InsertModel(MarkaNaVozilo mv);
+        void UpdateModel(MarkaNaVozilo mv);
+        void DeleteModel(int mid);
+        List<MarkaNaVozilo> GetModels();
+        List<MarkaNaVozilo> GetModelsByModelId(int ModelId);
+
+    }
+     public class MarkaNaVoziloRepository : IMarkaNaVoziloRepository
+    {
+         readonly StoredVehiclesDatabaseDbContext dbcontext;
+
+        public MarkaNaVoziloRepository()
+        {
+            dbcontext = new StoredVehiclesDatabaseDbContext();
+        }
+
+        public void InsertModel (MarkaNaVozilo mv)
+        {
+            dbcontext.MarkaNaVozilo.Add(mv);
+            dbcontext.SaveChanges();
+        }
+
+        public void UpdateModel (MarkaNaVozilo mv)
+        {
+            MarkaNaVozilo mvo = dbcontext.MarkaNaVozilo.Where(m => m.ModelId == m.ModelId).FirstOrDefault();
+            if(mvo != null)
+            {
+                mvo.ModelName = mv.ModelName;
+                dbcontext.SaveChanges();
+            }
+        }
+
+        public void DeleteModel (int dim)
+        {
+            MarkaNaVozilo mv = dbcontext.MarkaNaVozilo.Where(m => m.ModelId == dim).FirstOrDefault();
+            if (mv != null)
+            {
+                dbcontext.MarkaNaVozilo.Remove(mv);
+                dbcontext.SaveChanges();
+            }
+        }
+
+        public List<MarkaNaVozilo> GetModels()
+        {
+            List<MarkaNaVozilo> mv = dbcontext.MarkaNaVozilo.ToList();
+            return mv;
+
+        }
+
+        public List<MarkaNaVozilo> GetModelsByModelId(int ModelId)
+        {
+            List<MarkaNaVozilo> mv = dbcontext.MarkaNaVozilo.Where(m => m.ModelId == ModelId).ToList();
+            return mv;
+        }
+
+
+
+    }
+}
