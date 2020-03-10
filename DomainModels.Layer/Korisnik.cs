2961a9b96e8e015038f0ae61a5988d404b25028e
viksatheboss <61952531+viksatheboss@users.noreply.github.com>
2020-03-10 10:49:18 +0100

    Add files via upload

---
+++ DomainModels.Layer/Korisnik.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels.Layer
+{
+    public class Korisnik
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ DomainModels.Layer/MarkaNaVozilo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels.Layer
+{
+    public class MarkaNaVozilo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ModelId { get; set; }
+        public string ModelName { get; set; }
+    }
+}
